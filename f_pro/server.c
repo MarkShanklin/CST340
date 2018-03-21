221bdf44a1cb16a16e0f7ec83ee64312754323e5
masisaries <masisaries@gmail.com>
2018-03-21 02:45:26 -0700

    cr

--- f_pro/server.c
+++ f_pro/server.c
@@ -63,8 +63,8 @@ int main(int argc, char const *argv[])
     {
         valread = read( new_socket , buffer, 1024);
         printf("%s\n",buffer );
-        send(new_socket , hello , strlen(hello) , 0 );
-        printf("Hello message sent\n");
+        send(new_socket , buffer , strlen(buffer) , 0 );
+        printf("Data message sent\n");
     }
     return 0;
 };
